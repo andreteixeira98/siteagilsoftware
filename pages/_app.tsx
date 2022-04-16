a64e19833aaacd7fa2010378adc86cd57e0fd0fe
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-04-16 18:57:17 -0300

    modify _document.tsx fisrt build and deploy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
+import  GlobalStyle  from '../styles/global';
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+    
+
+  )
 }
 
 export default MyApp
---
+++ styles/global.ts
@@ -0,0 +1,14 @@
+import {createGlobalStyle} from "styled-components";
+
+export default createGlobalStyle`
+    *{
+        margin: 0;
+        padding:0;
+        box-sizing:border-box;
+    }
+    body{
+        background:#fff;
+        color:red;
+        font: 400 16px Roboto,  sans-serif;
+    }
+`;
